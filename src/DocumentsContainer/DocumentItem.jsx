bc7f05e65e69802eb58981319436230b514c6ca0
Danial Saleem <danialsaleem2010@gmail.com>
2020-03-23 02:06:54 +0500

    Document View Majorly Finalized. Some UI touches can be implemetented. Links should be added

--- src/DocumentsContainer/DocumentItem.jsx
+++ src/DocumentsContainer/DocumentItem.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Container, Row, Col} from "react-grid-system";
+import {Row, Col} from "react-grid-system";
 import MoreVerticalIcon from "@material-ui/icons/MoreVert";
 import DocumentIcon from "@material-ui/icons/Description";
+import TextIcon from "@material-ui/icons/TextFormat";
 import PeopleIcon from "@material-ui/icons/People";
 
+
 const DocumentItem = props => {
+    const styles = {
+        padding : "10px 0px"
+    };
+
     return (
-        <Container fluid>
+        <div style={styles}>
             <Row>
-                <Col sm={1}><DocumentIcon /></Col>
+                <Col sm={1}>{props.type === "doc" ? <DocumentIcon /> : <TextIcon />}</Col>
                 <Col sm={5}>
                     <Row justify="start">
                         <Col xs="content">
-                            Web Proposal
+                            {props.name}
                         </Col>
                         <Col xs={1}>
-                            <PeopleIcon />
+                            {props.isShared ? <PeopleIcon /> : null } 
                         </Col>
                     </Row>
                 </Col>
-                <Col sm={2}>Me</Col>
-                <Col sm={2}>4:48 PM</Col>
+                <Col sm={2}>{props.owner}</Col>
+                <Col sm={2}>{props.timeAccessed}</Col>
                 <Col sm={2}><MoreVerticalIcon /></Col>
             </Row>
-        </Container>
+        </div>
     );
 };
 
+DocumentItem.propTypes  = {
+    type: PropTypes.oneOf(['doc', 'txt']).isRequired,
+    name: PropTypes.string.isRequired,
+    isShared: PropTypes.bool.isRequired,
+    owner: PropTypes.string.isRequired,
+    timeAccessed: PropTypes.string.isRequired,
+}
+
+DocumentItem.defaultProps = {
+    type: "doc"
+}
 
 export default DocumentItem;
--- src/DocumentsContainer/DocumentsList.jsx
+++ src/DocumentsContainer/DocumentsList.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import DocumentItem from "./DocumentItem";
+import { Row, Col } from "react-grid-system";
 
 
-const DocumentList = () => <DocumentItem />
+const DocumentList = () => {
+    return (
+        <Row justify="center">
+        <Col xs={8}>
+            {[...Array(50).keys()].map((val, index) => 
+                <DocumentItem 
+                    type={index % 2 === 0? 'doc' : 'txt'}
+                    name={"File" + index}
+                    owner="me"
+                    isShared={index % 2 === 0}
+                    timeAccessed="4:53 PM"
+                />
+            )
+            }
+        </Col>
+        </Row>
+    )
+}
 
 
 export default DocumentList;
