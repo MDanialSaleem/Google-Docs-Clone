ff9827a0753f77e4b884056103cda35fa557621f
Danial Saleem <danialsaleem2010@gmail.com>
2020-03-22 23:59:21 +0500

    Template Document Card complete

--- src/DocumentsContainer/Documents.jsx
+++ src/DocumentsContainer/Documents.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import DocumentList from "./DocumentsList";
+import NewDocCardRow from "./NewDocCardRow";
 
-const Documnets = () => <DocumentList />;
-
+const Documnets = () => 
+<>
+    <NewDocCardRow />
+    <DocumentList />;
+</>
 
 export default Documnets;
---
+++ src/DocumentsContainer/NewDocCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from "prop-types";
+
+const styles = {
+    maxWidth : "170px"
+};
+
+const NewDocCard = props => {
+  return (
+    <div style={styles}>
+        <Card>
+            <CardMedia
+              component="img"
+              alt={props.title}
+              height="140"
+              image={props.imageUrl}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {props.title}
+              </Typography>
+            </CardContent>
+        </Card>
+    </div>
+  );
+}
+
+
+NewDocCard.propTypes = {
+    imageUrl : PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+};
+
+export default NewDocCard;
