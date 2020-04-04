95dcfaea9f92eacce1a10e703327a361d43979bd
Danial Saleem <danialsaleem2010@gmail.com>
2020-04-04 08:59:56 +0500

    Enabling Context in font changer and doc editor

--- src/EditorContainer/DocEditor.jsx
+++ src/EditorContainer/DocEditor.jsx
@@ -1,34 +1,44 @@
-import React, { useCallback, useMemo, useState } from 'react'
-import { Editable, withReact, Slate } from 'slate-react'
-import { createEditor } from 'slate'
-import { withHistory } from 'slate-history'
+import React, { useCallback, useMemo, useState } from "react";
+import { Editable, withReact, Slate } from "slate-react";
+import { createEditor } from "slate";
+import { withHistory } from "slate-history";
 import CustomElements from "./EditorUtils/CustomElements";
 import initialValue from "./EditorUtils/InitialValue";
-import EventHandlers from './EditorUtils/EventHandlers'
-import SubToolBar2 from './SubToolbar2'
-
-
+import EventHandlers from "./EditorUtils/EventHandlers";
+import SubToolBar2 from "./SubToolbar2";
+import EditorState from "./EditorContext/State";
 
 const RichTextExample = () => {
-  const [value, setValue] = useState(initialValue)
-  const renderElement = useCallback(props => <CustomElements.Element {...props} />, [])
-  const renderLeaf = useCallback(props => <CustomElements.Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+    const [value, setValue] = useState(initialValue);
+    const renderElement = useCallback(
+        (props) => <CustomElements.Element {...props} />,
+        []
+    );
+    const renderLeaf = useCallback(
+        (props) => <CustomElements.Leaf {...props} />,
+        []
+    );
+    const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
+    return (
+        <Slate
+            editor={editor}
+            value={value}
+            onChange={(value) => setValue(value)}
+        >
+            <EditorState>
+                <SubToolBar2 />
+                <Editable
+                    renderElement={renderElement}
+                    renderLeaf={renderLeaf}
+                    placeholder="A rich text editor"
+                    spellCheck
+                    autoFocus
+                    onKeyDown={(event) => EventHandlers.keyDown(event, editor)}
+                />
+            </EditorState>
+        </Slate>
+    );
+};
 
-  return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <SubToolBar2 />
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        placeholder="A rich text editor"
-        spellCheck
-        autoFocus
-        onKeyDown={event => EventHandlers.keyDown(event, editor)}
-      />
-    </Slate>
-  )
-}
-
-export default RichTextExample
+export default RichTextExample;
--- src/EditorContainer/FontChanger.jsx
+++ src/EditorContainer/FontChanger.jsx
@@ -3,21 +3,25 @@ import { useSlate } from "slate-react";
 import CustomHelpers from "./EditorUtils/CustomHelpers";
 import StyleConstants from "./EditorUtils/StyleConstants";
 import Fonts from "./EditorUtils/Fonts";
+import EditorContext from "./EditorContext/Context";
+
 
 const FontChanger = () => {
-    const editor = useSlate();
-    const [value, changeValue] = useState(Fonts.Arial);
-    let active = CustomHelpers.isMarkActive(editor, StyleConstants.FONT, Fonts.Arial);
-    console.log(active);
-    if(active && active !== value){
-        changeValue(active);
-    }
+    //const editor = useSlate();
+    //let active = CustomHelpers.isMarkActive(editor, StyleConstants.FONT, Fonts.Arial);
+
+    const editorContext = React.useContext(EditorContext);
+
+
+    // if(active && active !== value){
+    //     changeValue(active);
+    // }
     const handleChange = event => {
-        changeValue(event.target.value);
-        CustomHelpers.toggleMark(editor, StyleConstants.FONT, event.target.value);
+        editorContext.setFont(event.target.value);
+        //CustomHelpers.toggleMark(editor, StyleConstants.FONT, event.target.value);
     };
     return (
-        <select value={value} onChange={handleChange}>
+        <select value={editorContext[StyleConstants.FONT]} onChange={handleChange}>
             <option value={Fonts.Arial}>Arial</option>
             <option value={Fonts.Monospace}>Monospace</option>
             <option value={Fonts.TimesNewRoman}>TimesNewRoman</option>
