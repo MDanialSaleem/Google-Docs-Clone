1c4e314d38da3a5b810e5a2efd7105ac0eaacdaf
Danial Saleem <danialsaleem2010@gmail.com>
2020-05-10 23:21:24 +0500

    Setting up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,75 @@
-import React from 'react';
-import {BrowserRouter, Switch, Link, Route} from "react-router-dom";
-import * as Paths from "./Utils/RoutingConstants" ;
+import React from "react";
+//for redux
+import { Provider } from "react-redux"; //connects react with redux.
+import store from "./Store/store";
+//end redux
+//for routing
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
+import * as Paths from "./Utils/RoutingConstants";
+//end routing.
 import Home from "./HomeContainer/Home";
 import Documents from "./DocumentsContainer/Documents";
 import Editor from "./EditorContainer/Editor";
-import { Menu } from "semantic-ui-react";
 
 const style = {
-  backgroundColor: "black",
-  overflow: 'auto',
-  textAlign: 'center',
-  padding: 20,
-}
+    backgroundColor: "black",
+    overflow: "auto",
+    textAlign: "center",
+    padding: 20,
+};
 
 function App() {
-  return (
-    <BrowserRouter>
-        <div style={style}>
-          <Link id="fonts" style={{float: "left", display: "inline-block", textDecoration: "none", paddingRight: 20, color: "white"}}><b>KAGHAZ</b></Link>
-          <Link id="fonts" style={{float: "right", display: "inline-block", textDecoration: "none", paddingRight: 20, color: "white"}} to={Paths.Home}>Logout</Link>
-          <Link id="fonts" style={{float: "right", display: "inline-block", textDecoration: "none", paddingRight: 20, color: "white"}} to={Paths.Documents}>Documents</Link>
-        </div>
-      <Switch>
-        <Route path={Paths.Home} exact component={Home} />
-        <Route path={Paths.Documents} exact component={Documents} />
-        <Route path={Paths.Editor} exact component={Editor} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <div style={style}>
+                    <Link
+                        id="fonts"
+                        style={{
+                            float: "left",
+                            display: "inline-block",
+                            textDecoration: "none",
+                            paddingRight: 20,
+                            color: "white",
+                        }}
+                    >
+                        <b>KAGHAZ</b>
+                    </Link>
+                    <Link
+                        id="fonts"
+                        style={{
+                            float: "right",
+                            display: "inline-block",
+                            textDecoration: "none",
+                            paddingRight: 20,
+                            color: "white",
+                        }}
+                        to={Paths.Home}
+                    >
+                        Logout
+                    </Link>
+                    <Link
+                        id="fonts"
+                        style={{
+                            float: "right",
+                            display: "inline-block",
+                            textDecoration: "none",
+                            paddingRight: 20,
+                            color: "white",
+                        }}
+                        to={Paths.Documents}
+                    >
+                        Documents
+                    </Link>
+                </div>
+                <Switch>
+                    <Route path={Paths.Home} exact component={Home} />
+                    <Route path={Paths.Documents} exact component={Documents} />
+                    <Route path={Paths.Editor} exact component={Editor} />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ client/src/Store/store.js
@@ -0,0 +1,19 @@
+//pure redux thing, nothing to do with react.
+import { createStore, applyMiddleware } from "redux";
+//smth to do with redux devtools in the browser. needs more elaboration.
+import { composeWithDevTools } from "redux-devtools-extension";
+//for async stuff. write more on this later.
+import thunk from "redux-thunk";
+import rootReducer from "./Reducers.js/Root";
+
+const initialState = {}; //empty because our state lives in the separate reducers.
+
+const middlewares = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export default store;
